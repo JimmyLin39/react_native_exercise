4cf8a960ef16dc7366294a33f7ae224add029378
JimmyLin39 <jimmylin0309@email.com>
2018-01-25 22:37:53 -0800

    render images from hard-code data

--- App.js
+++ App.js
@@ -6,6 +6,8 @@
 
 import React, { Component } from 'react';
 import {
+  FlatList,
+  Image,
   Platform,
   StyleSheet,
   Text,
@@ -20,18 +22,57 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component<{}> {
+  _keyExtractor = (item, index) => item.id;
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <FlatList
+          data={[
+            {
+              "albumId": 1,
+              "id": 1,
+              "title": "accusamus beatae ad facilis cum similique qui sunt",
+              "url": "http://placehold.it/600/92c952",
+              "thumbnailUrl": "http://placehold.it/150/92c952"
+            },
+            {
+              "albumId": 1,
+              "id": 2,
+              "title": "reprehenderit est deserunt velit ipsam",
+              "url": "http://placehold.it/600/771796",
+              "thumbnailUrl": "http://placehold.it/150/771796"
+            },
+            {
+              "albumId": 1,
+              "id": 3,
+              "title": "officia porro iure quia iusto qui ipsa ut modi",
+              "url": "http://placehold.it/600/24f355",
+              "thumbnailUrl": "http://placehold.it/150/24f355"
+            },
+            {
+              "albumId": 1,
+              "id": 4,
+              "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+              "url": "http://placehold.it/600/d32776",
+              "thumbnailUrl": "http://placehold.it/150/d32776"
+            },
+            {
+              "albumId": 1,
+              "id": 5,
+              "title": "natus nisi omnis corporis facere molestiae rerum in",
+              "url": "http://placehold.it/600/f66b97",
+              "thumbnailUrl": "http://placehold.it/150/f66b97"
+            }
+          ]}
+          keyExtractor={this._keyExtractor}
+          renderItem={({item}) => 
+            <View>
+              <Text style={styles.text}>{item.title}</Text>
+              {/* load url with HTTPS */}
+              <Image source={{uri: item.url.slice(0, 4) + 's' + item.url.slice(4)}} style={{width: 400, height: 400}} />
+            </View>
+          }
+        />
       </View>
     );
   }
@@ -43,9 +84,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    marginTop: 20
   },
-  welcome: {
-    fontSize: 20,
+  text: {
+    fontSize: 15,
     textAlign: 'center',
     margin: 10,
   },
